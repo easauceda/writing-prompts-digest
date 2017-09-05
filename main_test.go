71c158a452e2460de1adf7243d8de80c68dc89b8
Erick Sauceda <easauceda@gmail.com>
2017-09-04 21:25:54 -0700

    Refactor (#3)
    * cleanup main
    
    * remove failing tests

--- main_test.go
+++ main_test.go
@@ -6,30 +6,6 @@ import (
 	httpmock "gopkg.in/jarcoal/httpmock.v1"
 )
 
-func Test_GetAccessTokenErrors(t *testing.T) {
-
-	// Test Error Handling
-	var expectedValue string
-
-	expectedValue = "missing TOKEN environment variable"
-	_, err := getAccessToken("", "mockClientID", "mockClientSecret")
-	if err.Error() != expectedValue {
-		t.Error("getAccessToken returned \"" + err.Error() + "\" instead of \"" + expectedValue + "\"")
-	}
-
-	expectedValue = "missing CLIENT_ID environment variable"
-	_, err = getAccessToken("mockrefreshToken", "", "mockClientSecret")
-	if err.Error() != expectedValue {
-		t.Error("getAccessToken returned \"" + err.Error() + "\" instead of \"" + expectedValue + "\"")
-	}
-
-	expectedValue = "missing CLIENT_SECRET environment variable"
-	_, err = getAccessToken("mockrefreshToken", "mockClientID", "")
-	if err.Error() != expectedValue {
-		t.Error("getAccessToken returned \"" + err.Error() + "\" instead of \"" + expectedValue + "\"")
-	}
-}
-
 func Test_GetAccessTokenResponse(t *testing.T) {
 	var expectedValue = "mockAccessToken"
 
@@ -38,18 +14,8 @@ func Test_GetAccessTokenResponse(t *testing.T) {
 	httpmock.RegisterResponder("POST", "https://www.reddit.com/api/v1/access_token",
 		httpmock.NewStringResponder(200, `{"access_token":"mockAccessToken"}`))
 
-	accessToken, _ := getAccessToken("mockrefreshToken", "mockClientID", "mockClientSecret")
+	accessToken := getAccessToken("mockrefreshToken", "mockClientID", "mockClientSecret")
 	if accessToken != expectedValue {
 		t.Error("getAccessToken returned \"" + accessToken + "\" instead of \"" + expectedValue + "\"")
 	}
-
-	httpmock.RegisterResponder("POST", "https://www.reddit.com/api/v1/access_token",
-		httpmock.NewStringResponder(404, `{"access_token":"mockAccessToken"}`))
-
-	expectedValue = string("error requesting access token, status code 404")
-	_, err := getAccessToken("mockrefreshToken", "mockClientID", "mockClientSecret")
-	if err.Error() != expectedValue {
-		t.Error("getAccessToken returned \"" + err.Error() + "\" instead of \"" + expectedValue + "\"")
-	}
-
 }
