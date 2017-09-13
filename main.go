11a86879726e0f1e702a525b0440d4ff16cd23e2
Erick Sauceda <easauceda@gmail.com>
2017-09-12 22:13:10 -0700

    distinguish daily emails

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 	digestBody := generateDigest(prompts)
 	digestRecipients := getRecipients()
 
-	digest := writingPromptEmail{emailAddress, digestRecipients, "New Stories for You!", digestBody}
+	digest := writingPromptEmail{emailAddress, digestRecipients, "New Stories for " + time.Now().Local().Format("Mon Jan 1, 2006"), digestBody}
 	sendEmails(digest)
 }
 
