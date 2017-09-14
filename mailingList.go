f23657576e3e9fef4a9b5ecc5de8fdef817bf9e6
Erick Sauceda <easauceda@gmail.com>
2017-09-13 22:22:36 -0700

    use postgres as the email store!

--- mailingList.go
+++ mailingList.go
@@ -1,11 +1,30 @@
 package main
 
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+)
+
 func getRecipients() []string {
-	return []string{
-		"easauceda@gmail.com",
+	var email string
+	emailAddresses := make([]string, 0)
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_HOST"))
+	if err != nil {
+		log.Fatal(err)
 	}
-}
 
-func generateMessages() {
-
+	rows, err := db.Query("SELECT email FROM userlist")
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&email)
+		if err != nil {
+			log.Fatal(err)
+		}
+		emailAddresses = append(emailAddresses, email)
+	}
+	return emailAddresses
 }
